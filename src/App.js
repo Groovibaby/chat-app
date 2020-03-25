d62827b4bf35f0379355bd9d7ba1a22a724bb60e
Thomas <thomas.lachieze@gmail.com>
2020-03-25 16:43:13 +0100

    List of items

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div>
-      <Contact
-        name="Groovibaby"
-        image="https://avatars0.githubusercontent.com/u/58938034?s=460&u=1be3bce5295ba29964a4d3167779de0cc0c1ae5b&v=4"
-        online
-      />
-      <Contact
-        name="Edith Morales"
-        image="https://randomuser.me/api/portraits/women/82.jpg"
-      />
-      <Contact
-        name="Francisco Graves"
-        image="https://randomuser.me/api/portraits/men/55.jpg"
-        online
-      />
+      <ContactList />
     </div>
   );
 }
